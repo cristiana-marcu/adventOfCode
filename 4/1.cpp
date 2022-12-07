d839ba4fad9b731ef0bc0ba2fb18d6dcf403e701
Cristiana nicoleta Marcu <cmarcu@c3r7s3.42madrid.com>
2022-12-07 15:36:42 +0100

    part one and two

---
+++ 4/1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+
+int get_char_index(std::string str, char c)
+{
+    for (int i = 0; i < str.length(); i++)
+        if (str[i] == c)
+            return (i);
+    return (0);
+}
+
+int main()
+{
+    std::string line;
+    std::ifstream infile("input.txt");
+    int total = 0;
+    int a, b, c, d;
+    std::string s1, s2;
+    int comma = 0; //index representing where the comma separating elfs is
+    int dash = 0;  //index representing where the dash representting the ranges is
+    //Input: 56-84,31-80
+    //       __ __ __ __
+    //       a  b ,c  d
+    //       _____,_____
+    //         s1    s2
+    while (std::getline(infile, line))
+    {
+        comma = get_char_index(line, ',');
+        s1 = line.substr(0, comma);
+        s2 = line.substr(comma + 1, line.length());
+        dash = get_char_index(s1, '-');
+        a = stoi(s1.substr(0, dash));
+        b = stoi(s1.substr(dash + 1, s1.length()));
+        dash = get_char_index(s2, '-');
+        c = stoi(s2.substr(0, dash));
+        d = stoi(s2.substr(dash + 1, s2.length()));
+        if ((a <= c && b >= d) || (c <= a && d >= b))
+            total++;
+    }
+    std::cout << total << std::endl;
+}
